94f3cf056843ba1841cae9724944b355a931eaa6
Paul Barajas <paul.barajas@linux.com>
2017-09-21 20:36:53 -0500

    BMP180 Sensor implementation

---
+++ tests/sensors_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+import unittest
+from DataQuatic.sensors import BMP180
+
+class TestStringMethods(unittest.TestCase):
+    def __init__(self):
+        self.bmp180 = BMP180.BMP180()
+
+    def test_bmp180(self):
+        self.assertTrue(self.bmp180.temperature)
+        self.assertTrue(self.bmp180.pressure)
+        self.assertTrue(self.bmp180.altitude)
