0628c2784da4540daa223996d38bccf57f1060a9
Erick <e2@live.com.mx>
2017-10-22 11:50:23 -0500

    Creation of DS18B20 Temperature sensor install script

---
+++ install/DS18B20_Temp_Sensor.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+#script to install dependencies for DS18B20 Temperature Sensor
+
+#author: Erick Noriega <e2@live.com.mx>
+#Usage: Execute the script without argument
+#Note: Make sure that you have python 3 installed and your 
+#git account configured in your OS.
+
+git clone ssh://ericknoriega@review.gerrithub.io:29418/PolixLab/DS18B20
+#compilelib makefile command
+#compile binary makefile command
+#install the binary in the desired location
