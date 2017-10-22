00d8ead88df18dc45dbe945e459a6483de3de8dc
Paul Barajas <paul.barajas@linux.com>
2017-10-22 12:13:11 -0500

    configuring SSL connection

---
+++ config/ssh_options.sh
@@ -0,0 +1,10 @@
+#This make fastest the SSL connection between raspberry and computer
+
+FILE="/etc/ssh/sshd_config"
+
+if ! grep -q "UseDNS" $FILE; then 
+    echo "Configuring file : $FILE"
+    echo "UseDNS=no" >> $FILE
+else
+    echo "Configuration was already applied."
+fi
